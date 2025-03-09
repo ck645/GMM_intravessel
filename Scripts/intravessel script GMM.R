8b64b4a8b17839ed5e4bff2639308fe4fb41e142
Catherine Klesner <catherine.klesner@gmail.com>
2025-03-09 12:53:21 +0000

    Update intravessel script GMM.R

--- Scripts/intravessel script GMM.R
+++ Scripts/intravessel script GMM.R
@@ -134,9 +134,75 @@ calibrate_harmonicpower_efourier(shapenorm2_99, nb.h = 20, plot = FALSE)
 calibrate_reconstructions_efourier(shapenorm2_99, range = 1:30)
 calibrate_deviations_efourier(shapenorm2_99)
 
-#compute efa shape with number of harmonics that account for 99.9% of variation (n=9)
+#compute efa shape with number of harmonics that account for 99.9% of variation (n=15)
 
-efashape_99 <- efourier(shapenorm2_99, nb.h = 9, smooth.it = 0, norm = TRUE)
+efashape_99 <- efourier(shapenorm2_99, nb.h = 15, smooth.it = 0, norm = TRUE)
+
+
+#visual examination of harmonics 
+
+coo_plot(shapenorm2_99[412], col = "transparent", centroid = TRUE,)
+
+CA230378_24 <- shapenorm2_99[412]
+
+efashape_CA230378_15 <- efourier(CA230378_24, nb.h = 15, smooth.it = 0, norm = TRUE)
+efashape_CA230378_20 <- efourier(CA230378_24, nb.h = 20, smooth.it = 0, norm = TRUE)
+efashape_CA230378_25 <- efourier(CA230378_24, nb.h = 25, smooth.it = 0, norm = TRUE)
+efashape_CA230378_30 <- efourier(CA230378_24, nb.h = 30, smooth.it = 0, norm = TRUE)
+efashape_CA230378_40 <- efourier(CA230378_24, nb.h = 40, smooth.it = 0, norm = TRUE)
+
+CA230378_15_xy <- efourier_i(efashape_CA230378_15, nb.h = 15, nb.pts = 200)
+CA230378_20_xy <- efourier_i(efashape_CA230378_20, nb.h = 20, nb.pts = 200)
+CA230378_25_xy <- efourier_i(efashape_CA230378_25, nb.h = 25, nb.pts = 200)
+CA230378_30_xy <- efourier_i(efashape_CA230378_30, nb.h = 30, nb.pts = 200)
+CA230378_40_xy <- efourier_i(efashape_CA230378_40, nb.h = 40, nb.pts = 200)
+
+CA230378_24 <- as.data.frame(CA230378_24)
+CA230378_15_xy <- as.data.frame(CA230378_15_xy)
+CA230378_20_xy <- as.data.frame(CA230378_20_xy)
+CA230378_25_xy <- as.data.frame(CA230378_25_xy)
+CA230378_30_xy <- as.data.frame(CA230378_30_xy)
+CA230378_40_xy <- as.data.frame(CA230378_40_xy)
+
+
+harmonics <- ggplot(CA230378_24, aes(x = V1, y = V2)) + 
+  geom_path(color = "black") + 
+  theme_minimal() +
+  labs(title = "CA230378_24", x = "x", y = "y") +
+  theme(legend.title = element_blank()) +
+  theme(legend.position = "bottom") +
+  scale_x_continuous(limits = c(-1.4, 1.4)) + 
+  scale_y_continuous(limits = c(-1.4, 1.4))
+
+# Add the Fourier shapes with different colors and legend
+harmonics + 
+  geom_path(data = CA230378_15_xy, aes(x = x, y = y, color = "15 harmonics")) +
+  geom_path(data = CA230378_20_xy, aes(x = x, y = y, color = "20 harmonics")) +
+  geom_path(data = CA230378_25_xy, aes(x = x, y = y, color = "25 harmonics")) +
+  geom_path(data = CA230378_30_xy, aes(x = x, y = y, color = "30 harmonics")) +
+  geom_path(data = CA230378_40_xy, aes(x = x, y = y, color = "40 harmonics")) +
+  scale_color_manual(values = c("15 harmonics" = "red", 
+                                "20 harmonics" = "orange", 
+                                "25 harmonics" = "yellow", 
+                                "30 harmonics" = "green", 
+                                "40 harmonics" = "blue")) +
+  labs(color = "Number of Harmonics")
+
+png(filename = "Figure3.png", width = 2400, height = 2400, res=300)
+plot(harmonics + 
+       geom_path(data = CA230378_15_xy, aes(x = x, y = y, color = "15 harmonics")) +
+       geom_path(data = CA230378_20_xy, aes(x = x, y = y, color = "20 harmonics")) +
+       geom_path(data = CA230378_25_xy, aes(x = x, y = y, color = "25 harmonics")) +
+       geom_path(data = CA230378_30_xy, aes(x = x, y = y, color = "30 harmonics")) +
+       geom_path(data = CA230378_40_xy, aes(x = x, y = y, color = "40 harmonics")) +
+       scale_color_manual(values = c("15 harmonics" = "red", 
+                                     "20 harmonics" = "orange", 
+                                     "25 harmonics" = "yellow", 
+                                     "30 harmonics" = "green", 
+                                     "40 harmonics" = "blue")) +
+       labs(color = "Number of Harmonics"))
+dev.off()
+
 
 #We will also run the analysis considering 30 harmonics.
 
